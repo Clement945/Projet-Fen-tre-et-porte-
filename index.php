c4540b58d6bf3fb1baf1ce009f01d126f54062fd
Clément Arki <clement.arki@hotmail.fr>
2023-11-22 14:44:54 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,227 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="style/style.css"/>
+    <title> Accueil - Fenêtre et porte  </title>
+</head>
+<body>
+
+
+
+<nav>
+        <ul>
+            <li><a href="index.php">Accueil</a></li>
+            <li><a href="fenetres.php">Fenêtres</a>
+                <ul class="sub-menu">
+                    <li><a href="fenetre_pvc.php">Fenêtres PVC</a></li>
+                    <li><a href="fenetre_bois.php">Fenêtres Bois</a></li>
+                    <li><a href="fenetre_aluminium.php">Fenêtres Aluminium</a></li>
+                </ul>
+            </li>
+            <li><a href="portes.php">Portes</a>
+            <ul class="sub-menu">
+                    <li><a href="fenetre_pvc.php">Portes Bois</a></li>
+                    <li><a href="fenetre_bois.php">Portes PVC</a></li>
+                    <li><a href="fenetre_aluminium.php">Portes Aluminium</a></li>
+                </ul>
+                </li>
+            <li><a href="volets.php">Volets</a></li>
+            <li><a href="baies_vitrees.php">Baies vitrées</a></li>
+            <li><a href="catalogues.php">Catalogues</a></li>
+            <li><a href="devis_gratuit.php">Devis gratuit</a></li>
+            <li><a href="contact.php">Contact</a></li>
+        </ul>
+
+
+    </nav>
+
+    <img src="./image/logo_hmf.jpg" title="Image HMF" alt="image HMF" id="image1"/>
+    <br/>
+    <br/>
+
+    <h1> Bienvenue sur le site de l'entreprise HMF </br>(Habitations Moderne de France) </h1>
+
+    <br/>
+    <br/>
+
+    <div class="image-container">
+    <img src="./image/volets-300x198.png" title="image volets" alt="image volets" id="image4"/>
+    <img src="./image/porte-PVC-entréé-décor-234x300.png" title="Image porte PVC" alt="image HMF" id="image2"/>
+    <img src="./image/fenetre-aluminium-décor.png" title="image fenetre aluminium" alt="image fenetre aluminium" id="image3"/>
+</div>
+
+    <br/>
+    <br/>
+
+
+    <div class="text-center">
+    <h2><u>VENEZ VOIR NOS ARTICLES DANS NOS MAGASINS :</u></h2>
+
+    <br/>
+
+    <ul id="taille-liste">
+        <li>- 8 Bis RUE HOCHE 94130 NOGENT-SUR-MARNE Ouvert du Lundi au vendredi de 9h30 à 12h30</li><br/>
+        <li>- 1 AVENUE DE STALINGRAD 94140 FONTENAY-SOUS-BOIS Ouvert du Lundi au Jeudi de 14h à 17h</li>
+    </ul>
+    </div>
+
+    <hr/>
+<div  class="text">
+<hr/>
+</br>
+
+    <p> Votre conseiller HMF vous accompagne tout au long de vos travaux de rénovation (fenêtres, portes, et volets en bois, PVC ou aluminium). </br></br>
+
+Nous sommes basés à Fontenay sous-bois et nous intervenons <strong>partout en île de France</strong>. Conseil produit dans le show-room, diagnostic à votre domicile, proposition commerciale personnalisée avec possibilités de financement, prises de côtes, fabrication française sur mesure, installation soignée et certifiée RGE, suivi après pose… </br></br>
+
+Nos équipes sont fières de respecter les 7 points de la charte d’excellence commerciale et technique ; c’est un gage de confiance entre vous et nous. Avec Arcades & Baies, pas de mauvaise surprise, du conseil produit à la pose des fenêtres neuves à Fontenay sous-bois. Nos équipes ont à cœur de vous écouter, de vous respecter et de vous satisfaire. Lancez-vous ! Vous avez trouvé le partenaire idéal pour <strong>l’installation des fenêtres PVC, Bois ou Aluminium à Fontenay sous-bois.</strong>
+</br></br>
+Avec nous, gagnez en confort, en sécurité, en isolation et en design. </p>
+
+</br>
+<hr/>
+</div>
+<hr/>
+
+<div class="image-container2">
+
+<h3> Fenêtre </h3>
+
+<img src="./image/Fenetre-bois-décor-300x283.png" title="image fenetre bois" alt="image fenetre bois" id="image6"/>
+
+</div>
+
+</br>
+
+<div class="listepuce">
+    <ul>
+        <li>Faites le choix d’une <strong>Fenêtre en bois</strong> pour une isolation renforcée, une ergonomie totale et le choix de l’esthétique (traditionnelle ou contemporaine).</li></br>
+        <li>Faites le choix d’une <strong>Fenêtre en PVC</strong> pour une sécurité haut de gammes, une isolation renforcée, et une ergonomie totale.</li></br>
+        <li id="listecentre1">Faites le choix d’une <strong>Fenêtre en aluminium</strong> pour une fenêtre élégante, une sécurité haut de gamme,</br> une isolation renforcée, une ergonomie total et un choix personnel de l’esthétique.</li>
+    </ul>
+
+</div>
+
+<div class="bouton-fenetre">
+
+<a href="fenetres.php" class="bouton">Choix des fenêtres</a>
+
+</div>
+
+</br>
+</br>
+
+<hr>
+
+
+<div class="image-container2">
+
+<h3> Portes </h3>
+
+<img src="./image/portes.png" title="image porte" alt="image porte" id="image6"/>
+
+</div>
+
+</br>
+
+<div class="listepuce">
+    <ul>
+        <li id="listecentre2"><strong>Notre collection de portes d’entrée</strong> : un univers d’exception, dans lequel nos matériaux rencontrent la couleur, le verre, le métal, dans des formes inédites et côtoient des techniques inventives. </li></br>
+        <li><strong>Arcades et Baies signe pour vous des créations exclusives.</strong> Peaufinez chaque détail, nous fabriquons.</li></br>
+    <li> <em>Chez nous le <strong>« sur-mesure »</strong> n’est pas un slogan. L’installation d’une porte chez Arcades & Baies, c’est une <strong>véritable philosophie d’entreprise.</strong></em> </li></br>
+    <li>Optez pour la gamme <strong>portes en bois</strong> Pour une ergonomie total</li></br>
+    <li>Optez pour la gamme <strong>porte en PVC</strong></li></br>
+    <li>Optez pour la gamme <strong>porte en aluminium</strong></li></br>
+
+    
+    </ul>
+</div>
+
+<div class="bouton-fenetre">
+
+<a href="volets.php" class="bouton">Choix des portes</a>
+
+</div>
+
+</br>
+
+<hr>
+
+
+<div class="image-container2">
+
+<h3> Volets </h3>
+
+<img src="./image/volets.png" title="image baies vitrées" alt="image baies vitrées" id="image6"/>
+
+</div>
+
+</br>
+
+<div class="listepuce">
+    <ul>
+        <li id="listecentre2">Avec les <strong>volets roulants intégrés</strong> ARCADES ET BAIES, l’entreprise HMF à Fontenay sous bois vous assure la fourniture et la pose de ces fermetures indispensables pour votre confort et votre sécurité. En PVC ou en ALUMINIUM.</li></br>
+        <li><strong>Les volets roulants peuvent être motorisés</strong> selon vos besoins et sont des éléments esthétiques adaptés à tous types de construction.</li></br>
+
+
+    
+    </ul>
+</div>
+
+<div class="bouton-fenetre">
+
+<a href="baies_vitrees.php" class="bouton">Choix des volets</a>
+
+</div>
+
+
+</br>
+
+<hr>
+
+<div class="image-container2">
+
+<h3> Baies vitrées</h3>
+
+<img src="./image/baies_vitrees.png" title="image volets" alt="image volets" id="image6"/>
+
+</div>
+
+</br>
+
+<div class="listepuce">
+    <ul>
+        <li id="listecentre2"><strong>La baie vitrée coulissante ARCADES ET BAIES installée par HMF à Fontenay sous bois</strong> donne la priorité à la lumière pour votre maison. Les performances thermiques de nos baies participent également activement aux économies d’énergie. </li></br>
+        <li id="listecentre2">Fabriquées en France, à vos mesures, elles répondent aux normes les plus strictes en vigueur et sont éligibles au crédit d’impôt (selon legislation en vigeur). </br><strong>La baie coulissante en aluminium à Fontenay sous bois</strong>, se décline dans une multitude de couleurs satinées ou sablées… Faites votre choix. </li></br>
+
+
+    
+    </ul>
+</div>
+
+<div class="bouton-fenetre">
+
+<a href="fenetres.php" class="bouton">Choix des baies vitrées</a>
+
+</div>
+
+
+</br>
+
+</br>
+
+</br>
+
+
+<footer>
+        <p id="footer">
+            &copy; <a href="mentions_legalesv2.pdf" target="_blank">Mentions légales</a> - <a href="politique_de_confidentialite.pdf" target="_blank">Politique de confidentialité</a> - <a href="gestion_des_cookies.pdf" target="_blank">Gestion des cookies</a> - <a href="droitimage.pdf" target="_blank">Droit à l'image</a>
+        </p>
+    </footer>
+
+
+    
+</body>
+</html>
