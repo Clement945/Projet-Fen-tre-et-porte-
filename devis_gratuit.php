0aca20bc7b2232ae5b2bb829769b709b82e575a1
Clément Arki <clement.arki@hotmail.fr>
2023-12-04 16:56:37 +0100

    19ème commit

--- devis_gratuit.php
+++ devis_gratuit.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" type="text/css" href="style/contact.css"/>
+    <link rel="stylesheet" type="text/css" href="style/devis_gratuit.css"/>
 
     <title>Devis gratuit - Fenêtre et porte</title>
 </head>
@@ -43,16 +43,79 @@
     <br/>
     <br/>
 
-    <h1 id="fenetres">Devis gratuit</h1>
+    <h1 id="fenetres">DEVIS GRATUIT</h1>
     <br/>
     <br/>
 
-
-
-
+   
 
     <hr/>
 
+    <div class="text">
+    <p> Commencez votre projet dès aujourd'hui en demandant un devis gratuit. <br/>Notre équipe dédiée est prête à transformer vos idées en réalité, 
+        offrant des solutions personnalisées <br/>pour répondre à vos besoins spécifiques.</p>
+
+</div>
+
+<hr/>
+<br/>
+
+
+
+
+<form action="traitement.php" method="post" class="form-container">
+<fieldset>
+    <h2 id="h3_milieu"> Veuillez remplir les champs ci-dessous </h2>
+  <div class="form-group">
+    <label for="nom">Nom :</label>
+    <input type="text" id="nom" name="nom" required>
+  </div>
+
+  <div class="form-group">
+    <label for="prenom">Prénom :</label>
+    <input type="text" id="prenom" name="prenom" required>
+  </div>
+
+  <div class="form-group">
+    <label for="telephone">Téléphone :</label>
+    <input type="tel" id="telephone" name="telephone" required>
+  </div>
+
+  <div class="form-group">
+    <label for="email">E-mail :</label>
+    <input type="email" id="email" name="email" required>
+  </div>
+
+  <div class="form-group">
+    <label for="adresse">Adresse :</label>
+    <input type="text" id="adresse" name="adresse">
+  </div>
+
+  <div class="form-group">
+    <label for="codePostal">Code Postal :</label>
+    <input type="text" id="codePostal" name="codePostal">
+  </div>
+
+  <div class="form-group">
+    <label for="ville">Ville :</label>
+    <input type="text" id="ville" name="ville">
+  </div>
+
+  <div class="form-group">
+    <label for="message">Message :</label>
+    <textarea id="message" name="message" rows="4" cols="50"></textarea>
+  </div>
+
+  <div class="form-group">
+    <input type="submit" value="Envoyer">
+  </div>
+  </fieldset>
+</form>
+
+
+
+<br/>
+<hr/>
 <br/>
 <br/>
 
---
+++ traitement.php
@@ -0,0 +1,135 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="style/traitement.css"/>
+
+    <title>Devis gratuit - Fenêtre et porte</title>
+</head>
+<body>
+
+
+
+
+<nav>
+        <ul>
+            <li><a href="index.php">Accueil</a></li>
+            <li><a href="fenetres.php">Fenêtres</a>
+                <ul class="sub-menu">
+                    <li><a href="fenetre_bois.php">Fenêtres Bois</a></li>
+                    <li><a href="fenetre_pvc.php">Fenêtres PVC</a></li>
+                    <li><a href="fenetre_aluminium.php">Fenêtres Aluminium</a></li>
+                </ul>
+            </li>
+            <li><a href="portes.php">Portes</a>
+            <ul class="sub-menu">
+                    <li><a href="porte_bois.php">Portes Bois</a></li>
+                    <li><a href="porte_pvc.php">Portes PVC</a></li>
+                    <li><a href="porte_aluminium.php">Portes Aluminium</a></li>
+                </ul>
+                </li>
+            <li><a href="volets.php">Volets</a></li>
+            <li><a href="baies_vitrees.php">Baies vitrées</a></li>
+            <li><a href="catalogues.php">Catalogues</a></li>
+            <li><a href="devis_gratuit.php">Devis gratuit</a></li>
+            <li><a href="contact.php">Contact</a></li>
+        </ul>
+
+ 
+    </nav>
+
+    <img src="./image/logo_hmf.jpg" title="Image HMF" alt="image HMF" id="image1"/>
+    <br/>
+    <br/>
+
+    <h1 id="fenetres">Devis gratuit</h1>
+    <br/>
+    <br/>
+
+
+    <?php
+try {
+    $bdd = new PDO('mysql:host=localhost;dbname=HMF;charset=utf8','root','');
+    $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    // echo("<p id='reussi'>Connexion réussie !</p><br/>");
+} catch (Exception $e) {
+    die('Erreur de connexion : ' . $e->getMessage());
+}
+
+try {
+    if (!empty($_POST['nom']) && !empty($_POST['prenom']) && !empty($_POST['telephone']) && !empty($_POST['email'])) {
+        // Récupération des valeurs du formulaire
+        $nom = htmlspecialchars(addslashes($_POST['nom']));
+        $prenom = htmlspecialchars(addslashes($_POST['prenom']));
+        $telephone = htmlspecialchars(addslashes($_POST['telephone']));
+        $email = htmlspecialchars(addslashes($_POST['email']));
+        $adresse = htmlspecialchars(addslashes($_POST['adresse']));
+        $code_postal = htmlspecialchars(addslashes($_POST['codePostal']));
+        $ville = htmlspecialchars(addslashes($_POST['ville']));
+        $message = htmlspecialchars(addslashes($_POST['message']));
+
+        // Insertion des données dans la table devis_gratuit
+        $req = $bdd->prepare('INSERT INTO devis_gratuit(devis_gratuit_nom, devis_gratuit_prenom, devis_gratuit_telephone, devis_gratuit_email, devis_gratuit_adresse, devis_gratuit_code_postal, devis_gratuit_ville, devis_gratuit_message) 
+                            VALUES(:nom, :prenom, :telephone, :email, :adresse, :code_postal, :ville, :message)');
+
+        $req->execute(array(
+            'nom' => $nom,
+            'prenom' => $prenom,
+            'telephone' => $telephone,
+            'email' => $email,
+            'adresse' => $adresse,
+            'code_postal' => $code_postal,
+            'ville' => $ville,
+            'message' => $message
+        ));
+
+        echo('<p class="nouveau">Votre devis a été envoyé !<br/><br/> Il sera traité dans les plus brefs délais.
+
+        <br/><br/><br/></p>
+        
+        <hr/>
+        <p class="nouveau" >Voici les données envoyées :</p><br/><br/>' .
+            '<p id="données">Le nom : ' . $_POST['nom'] . '<br/> 
+            Le prénom : ' . $_POST['prenom'] . '<br/>
+            Le téléphone : ' . $_POST['telephone'] . '<br/>
+            L\'email : ' . $_POST['email'] . '<br/>
+            L\'adresse : ' . $_POST['adresse'] . '<br/>
+            Le code postal : ' . $_POST['codePostal'] . '<br/>
+            La ville : ' . $_POST['ville'] . '<br/>
+            Le message : ' . $_POST['message'] . '</p>'.
+            '<br/><br/><p class="nouveau2">Merci pour votre envoi !</p>');
+
+    } else {
+        echo "<p id='remplir'>Veuillez remplir tous les champs obligatoires. </p>";
+    }
+} catch (Exception $e) {
+    die('Erreur d\'insertion avec variable ou enregistrement déjà existant'. $e->getMessage());
+}
+?>
+
+<br/>
+<br/>
+
+    <hr/>
+<br/>
+<br/>
+
+    <p id="index"><a href="#">Cliquez-ici pour remonter</a></p>
+
+
+<br/>
+<br/>
+
+
+
+    <footer>
+        <p id="footer">
+            &copy; <a href="mentions_legales.pdf" target="_blank">Mentions légales</a> - <a href="politique_de_confidentialite.pdf" target="_blank">Politique de confidentialité</a> - <a href="gestion_des_cookies.pdf" target="_blank">Gestion des cookies</a> - <a href="droitimage.pdf" target="_blank">Droit à l'image</a>
+        </p>
+    </footer>
+
+
+
+</body>
+</html>
